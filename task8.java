1762d9d9ca2b5b5d465cd2677612979a27989f71
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-14 16:57:30 +0000

    Write a program to Create a List with String more than 2 characters

---
+++ task8.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+public class task8 {
+     public static void main(String[] args) {
+       
+            Supplier < LocalDateTime > supplier = () -> LocalDateTime.now();
+            System.out.println(supplier.get());
+        }
+    
+}
