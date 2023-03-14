7145c59e64df41fab5b6192119823a671622fd94
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-14 14:38:57 +0000

    Write a program to iterate over list using lambda expression

---
+++ task5.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class task5 {
+    public static void main(String[] args) {
+        List <Integer> a= Arrays.asList(1,1,2,3,6,8,5,3,5);
+        a.forEach(e->System.out.println(e));
+
+    }
+}
