6b7441d97d32b5a10822bcd641276885477a821a
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-14 12:32:46 +0000

    Write a Functional Interface with method signature - String reverseString(String s) and implement interface using lambda expression

---
+++ task2.java
@@ -0,0 +1,22 @@
+public interface task2{
+    String reverseString(String s);
+}
+
+class Example{
+    public static void main(String[] args) {
+      
+     task2 sr= (s)-> {
+        StringBuilder sb=new StringBuilder(s);
+        return sb.reverse().toString();
+     };
+
+     String or="Omkar jadhav";
+      String re= sr.reverseString(or);
+      System.out.println("original String"+or);
+      System.out.println("reverse String"+re);
+     }
+    }
+       
+      
+
+    }
